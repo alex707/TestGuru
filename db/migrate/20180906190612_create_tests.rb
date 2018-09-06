64be42539f9dc8445a00e05b78975e269830b304
Alex <alex.grigorenko2942@gmail.com>
2018-09-06 22:27:08 +0300

    added models test, cat, question, answer, user. added default and null:false

---
+++ db/migrate/20180906190612_create_tests.rb
@@ -0,0 +1,11 @@
+class CreateTests < ActiveRecord::Migration[5.2]
+  def change
+    create_table :tests do |t|
+      t.string :title
+      t.integer :level, default: 0
+      t.integer :category_id, null: false
+
+      t.timestamps
+    end
+  end
+end
