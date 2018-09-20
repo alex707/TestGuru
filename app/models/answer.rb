5e5e3e280adc47a21ccd6d5a3a342e0a68cfd9fa
Alex <alex.grigorenko2942@gmail.com>
2018-09-20 20:33:50 +0300

    scopes refactored

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -2,13 +2,13 @@ class Answer < ApplicationRecord
   belongs_to :question
 
   validates :body, presence: true
-  validate :check_allow_count, on: :create
+  validate :check_count, on: :create
 
   scope :correct, -> { where(correct: true) }
 
   private
 
-  def check_allow_count
+  def check_count
     cnt = self.question.answers.count
     if cnt >= 4
       errors.add(:answers, 'count of answers must be less then 5 (i.e. 1..4)')
--- app/models/test.rb
+++ app/models/test.rb
@@ -1,6 +1,4 @@
 class Test < ApplicationRecord
-  DIFFICULTY_LEVEL = { easy: 0..1, normal: 2..4, hard: 5..Float::INFINITY }
-
   belongs_to :category
   belongs_to :author, class_name: 'User'
 
@@ -12,5 +10,7 @@ class Test < ApplicationRecord
   validates :level, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   scope :by_category, -> (name) { where(categories: { title: name }).joins(:category).order(id: :desc) }
-  scope :difficulty_level, -> (level) { where(level: DIFFICULTY_LEVEL[level]) }
+  scope :easy, -> { where(level: 0..1) }
+  scope :normal, -> { where(level: 2..4) }
+  scope :hard, -> { where(level: 5..Float::INFINITY) }
 end
