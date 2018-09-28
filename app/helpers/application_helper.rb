c60a89961da1cc38c0ad7090e3cca1167e457587
Alex <alex.grigorenko2942@gmail.com>
2018-09-28 21:39:20 +0300

    added views for main controllers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def current_year
+    Time.now.year
+  end
+
+  def github_url(author, repo)
+    "<a target=\"_blank\" href=\"http://github.com/#{repo}\">#{author}</a>".html_safe
+  end
 end
--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,2 +1,9 @@
 module QuestionsHelper
+  def question_header(action, test)
+    if action == 'create'
+      "Create New #{test.title} Question"
+    elsif action == 'edit'
+      "Edit #{test.title} Question"
+    end
+  end
 end
